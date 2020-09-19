8aade9959363b0db344b8b0516ecd36435e029ed
MurtadhaMohammed <mumu9943@gmail.com>
2020-09-19 17:55:44 +0300

    add props

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { Row, Col, Input, Button, Card } from "antd";
-import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+import TodoItem from "../components/todoItem";
+import TodoForm from "../components/todoForm";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
@@ -15,67 +15,15 @@ const Home = () => {
 
   return (
     <div className="container">
-      <div className="form">
-        <Row gutter={20}>
-          <Col span={18}>
-            <Input
-              placeholder="Write any text..."
-              value={todoName}
-              onChange={(e) => setTodoName(e.target.value)}
-            />
-          </Col>
-          <Col span={4}>
-            <Button
-              style={{ width: "100%" }}
-              type="primary"
-              icon={<PlusOutlined />}
-              onClick={() => {
-                setTodos([...todos, todoName]);
-                setTodoName("");
-                localStorage.setItem(
-                  "todos",
-                  JSON.stringify([...todos, todoName])
-                );
-              }}
-            >
-              Add
-            </Button>
-          </Col>
-          <Col span={2}>
-            <Button
-              style={{ width: "100%" }}
-              type="primary"
-              icon={<DeleteOutlined />}
-              onClick={() => {
-                 setTodos([])
-                 localStorage.setItem("todos", "")
-              }}
-
-              danger
-            />
-          </Col>
-        </Row>
-      </div>
-
+      <TodoForm
+        todoName={todoName}
+        setTodoName={setTodoName}
+        todos={todos}
+        setTodos={setTodos}
+      />
       <div className="todo-list">
         {todos.map((item, index) => (
-          <Card hoverable key={index}>
-            <Row>
-              <Col span={23}>{item}</Col>
-              <Col span={1}>
-                <Button
-                  danger
-                  type="primary"
-                  shape="circle"
-                  icon={<DeleteOutlined />}
-                  onClick={() => {
-                    setTodos(todos.filter((el) => el !== item));
-                    localStorage.setItem("todos", JSON.stringify(todos.filter((el) => el !== item)))
-                  }}
-                />
-              </Col>
-            </Row>
-          </Card>
+          <TodoItem key={index} item={item} todos={todos} setTodos={setTodos} />
         ))}
       </div>
     </div>
